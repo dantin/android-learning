c0ee70c29a19e5d4bc91c743f69686df4a98e494
David Ding <chengjie.ding@gmail.com>
2014-06-09 17:09:50 +0800

    basic action bar

---
+++ MyActionBar/src/com/example/MyActionBar/MyActivity.java
@@ -0,0 +1,61 @@
+package com.example.MyActionBar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+
+    private TextView message;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initializeApp();
+    }
+
+    private void initializeApp() {
+        this.message = (TextView) findViewById(R.id.message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openSearch() {
+        message.setText("Search");
+    }
+
+    public void openSettings() {
+        message.setText("Settings");
+    }
+
+
+}
